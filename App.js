52edda0184c7f033b230d80a01a8802a015237a9
Jose Arroyo <jc.arroyo10@gmail.com>
2018-08-13 22:48:46 -0500

    Se agrega imagen a ListItem

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 import PlaceList from './src/components/PlaceList/PlaceList'
 import PlaceInput from './src/components/PlaceInput/PlaceInput'
+import placeImage from './src/assets/picture.jpg';
 
 export default class App extends React.Component {  
   state = {    
@@ -12,16 +13,21 @@ export default class App extends React.Component {
   placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat(
+          {
+            key: Math.random(), 
+            name: placeName,
+            image: placeImage
+          })
       }
     })
   }
 
-  placeDeletedHandler = index => {
+  placeDeletedHandler = key => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place, i) => {
-          return i !== index;
+        places: prevState.places.filter(place => {
+          return place.key !== key;
         })
       }
     })
